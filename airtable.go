0e92587f41f932c2fa4f43a220777115adddf96d
vikas <bizpowersolution@gmail.com>
2022-12-11 11:07:19 +0530

    integrations

---
+++ airtable.go
@@ -0,0 +1,4 @@
+package database_integrations
+
+//client := airtable.NewClient("your_api_token")
+//https://github.com/mehanizm/airtableg
---
+++ go.mod
@@ -0,0 +1,5 @@
+module database_integrations
+
+go 1.18
+
+require github.com/mehanizm/airtable v0.2.6 // indirect
